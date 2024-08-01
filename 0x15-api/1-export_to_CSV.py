e159aa04ddb50b541e9317861687509f91db64cd
Joshua <atsujoshua67@gmail.com>
2024-07-31 23:36:22 -0700

    in CSV format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Using what you did in the task #0,
-extend your Python script to export data in the JSON format.
+extend your Python script to export data in the CSV format.
 
 Requirements:
 
 Records all tasks that are owned by this employee
 Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
-File name must be: USER_ID.json
+File name must be: USER_ID.csv
 """
 import csv
 import requests
