1fee82f9033c3cc5f93b6b26ce559e39a5d9aea0
Joshua <atsujoshua67@gmail.com>
2024-08-23 22:48:06 +0000

    fix request max problem

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Fix the problem where the server cannot process many requests
+
+exec {'modify max open file limit setting':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
