39b9381a7cebd8c2cb652d1f39fe86781849f235
Joshua <atsujoshua67@gmail.com>
2024-08-25 06:53:19 +0000

    User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 # this finction adds a text to a file 
 
 exec {'increase-hard-file-limit-for-holberton-user':
-command => 'sed -i "/holberton hard nofile/s/4/50000/" /etc/security/limits.conf',
+command => 'sed -i "57 s/4/50000/" /etc/security/limits.conf',
 path    => '/usr/local/bin/:/bin/'
 }
 exec {'increase-soft-file-limit-for-holberton-user':
-command => 'sed -i "/holberton soft nofile/s/5/50000/" /etc/security/limits.conf',
+command => 'sed -i "56 s/5/50000/" /etc/security/limits.conf',
 path    => '/usr/local/bin/:/bin/'
 }
